804fe1d857fb634ee22b61b91beeec63cfb33ee3
Dwayne Hovington <dhov92@outlook.com>
2021-07-18 15:30:24 -0400

    .

---
+++ Develop/public/budget.js
@@ -0,0 +1,66 @@
+let db;
+
+//  create an indexed database to manage budget data
+const request = indexedDB.open("budgetDB", 1);
+
+request.onupgradeneeded = function (event) {
+
+    //  create an empty/open object
+    const db = event.target.result;
+    db.createObjectStore("open", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    database = event.target.result;
+
+    //  validate the app is available and go to the database or throw an error in the console if there is an issue
+    if (navigator.onLine) {
+        gotoDB();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("ERROR: " + event.target.errorCode);
+};
+
+//  set the database for updates and add data
+function saveData(data) {
+    const transaction = db.transaction(["open"], "readwrite");
+
+    const store = transaction.objectStore("open");
+
+    store.add(data);
+}
+
+//  gather database information for use
+function gotoDB() {
+    const transaction = db.transaction(["open"], "readwrite");
+
+    const store = transaction.objectStore("open");
+
+    const pullAllData = store.pullAllData();
+
+    //  verifies successful pull and sets for update/use/closure
+    pullAllData.onsuccess = function () {
+        if (pullAllData.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(pullAllData.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["open"], "readwrite");
+
+                    const store = transaction.objectStore("open");
+
+                    store.clear();
+                });
+        }
+    };
+}
+//  listener for app availability and DB start
+window.addEventListener("online", gotoDB);
